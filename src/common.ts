5d47b29c094f201e244c4ed2dd1498466c98b0a6
MendyBerger <12537668+MendyBerger@users.noreply.github.com>
2026-08-12 17:18:05 -0400

    refactor: Make the implementations stricter

--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,14 @@
+// Rewrites methods as function properties so params are checked strictly (no bivariance)
+//
+// Plain `implements I` allows this, while implements Strictify<I> would error
+// `interface I { foo(n: number | undefined): void }`
+// `class C implements I { foo(n: number) {} }`
+export type Strict<T> = {
+    [K in keyof T]: T[K] extends (...args: infer A) => infer R
+        ? (...args: A) => R
+        : T[K];
+};
+
 export class Todo extends Error {
     constructor() {
         super("TODO: not yet implemented");
